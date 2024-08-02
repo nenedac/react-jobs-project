d20776f53b92719b2eb0c6512a5d8a556352e34e
Deyner Cruz <nenedac98@gmail.com>
2024-08-02 04:36:03 -0600

    refactor: moved job listings from app.jsx to their own files
    - created job listings component to display job data from jobs.json
    - implemented job listing component to show individual job details
    - used tailwind classes for consistent styling and layout
    - added indentation across the board for better readability

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,7 +1,6 @@
 import Card from './Card';
 const HomeCards = () => {
   return (
-
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
@@ -33,7 +32,6 @@ const HomeCards = () => {
         </div>
       </div>
     </section >
-
   );
 };
 
