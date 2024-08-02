2a82f251cbb26ed7f266e4c37925ae51fc858058
Deyner Cruz <nenedac98@gmail.com>
2024-08-02 02:51:29 -0600

    refactor: hero components and homecards into their own files
    -imports the necesary files into App.jsx for proper functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,8 @@
+import logo from '../assets/images/logo.png';
+
 const Navbar = () => {
     return (
         <>
-
             <nav className="bg-indigo-700 border-b border-indigo-500">
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                     <div className="flex h-20 items-center justify-between">
@@ -12,7 +13,7 @@ const Navbar = () => {
                             <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
                                 <img
                                     className="h-10 w-auto"
-                                    src="images/logo.png"
+                                    src={logo} /* used to make a dynamic route */
                                     alt="React Jobs"
                                 />
                                 <span className="hidden md:block text-white text-2xl font-bold ml-2"
