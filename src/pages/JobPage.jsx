9bd30cac83d4247c9a69cf157138d084d4978fe6
Deyner Cruz <nenedac98@gmail.com>
2024-08-04 04:40:31 -0600

    feat: add job edit and delete functionality
    - implemented job edit and delete features with Tailwind styling
    - configured routes in app.jsx and API functions for job management

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,14 +1,28 @@
 import { useParams, useLoaderData, useNavigate } from 'react-router-dom';
 import { FaArrowLeft, FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const job = useLoaderData();
 
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      'Are you sure you want to delete this listing?'
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    toast.success('Job deleted successfully');
+
+    navigate('/jobs');
+  };
 
   return (
-
     <>
       <section>
         <div className='container m-auto py-6 px-6'>
@@ -73,15 +87,17 @@ const JobPage = () => {
                 </p>
               </div>
 
-              <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <h3 className="text-xl font-bold mb-6">Manage Job</h3>
+              <div className='bg-white p-6 rounded-lg shadow-md mt-6'>
+                <h3 className='text-xl font-bold mb-6'>Manage Job</h3>
                 <Link
                   to={`/edit-job/${job.id}`}
-                  className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
-                >Edit Job</Link
+                  className='bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
                 >
+                  Edit Job
+                </Link>
                 <button
-                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                  onClick={() => onDeleteClick(job.id)}
+                  className='bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block'
                 >
                   Delete Job
                 </button>
